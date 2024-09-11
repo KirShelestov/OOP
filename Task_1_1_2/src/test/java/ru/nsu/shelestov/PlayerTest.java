cfb4492cebda1f355be551f6d43ff0d5ba932d4c
Kirill <k.shelestov@g.nsu.ru>
2024-09-11 16:54:05 +0700

    Added test for Player

---
+++ Task_1_1_2/src/test/java/ru/nsu/shelestov/PlayerTest.java
@@ -0,0 +1,43 @@
+package ru.nsu.shelestov;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+/**
+ * Тестовый класс для проверки функциональности класса Player.
+ */
+public class PlayerTest {
+    private final InputStream originalIn = System.in;
+    private Player player;
+    private Deck deck;
+    private Deck discard;
+
+    /**
+     * инициализация.
+     */
+    @BeforeEach
+    public void setUp() {
+        player = new Player();
+        deck = new Deck();
+        discard = new Deck();
+    }
+
+    /**
+     * Проверяет, что после выбора "0" у игрока не добавляется карта в руку.
+     */
+    @Test
+    public void testRevealHand() {
+        Card card = new Card(Suit.DIAMOND, CardInfo.SIX);
+        Deck deck = new Deck(true);
+        player.getHand().takeCardFromDeck(deck);
+
+        player.revealHand();
+        assertEquals(1, player.getHand().getSize());
+    }
+}
