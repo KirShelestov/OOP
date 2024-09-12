4dcd60c8e31e23e2f7eb3103f92bb23f666dd756
Kirill <k.shelestov@g.nsu.ru>
2024-09-12 09:13:04 +0700

    Added test for Game

---
+++ Task_1_1_2/src/test/java/ru/nsu/shelestov/GameTest.java
@@ -0,0 +1,55 @@
+package ru.nsu.shelestov;
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+public class GameTest {
+    @Test
+    public void testGameStarts() {
+
+        InputStream originalIn = System.in;
+
+        try {
+            ByteArrayInputStream in = new ByteArrayInputStream("0\n".getBytes());
+            System.setIn(in);
+            Game game = new Game();
+
+            assertTrue(true);
+        } catch (Exception e) {
+            ByteArrayInputStream inn = new ByteArrayInputStream("нет\n".getBytes());
+            System.setIn(inn);
+        } finally {
+            System.setIn(originalIn);
+        }
+        assertTrue(true);
+    }
+    @Test
+    public void testGameStarts1() {
+
+        InputStream originalIn = System.in;
+
+        try {
+            ByteArrayInputStream in = new ByteArrayInputStream("1\n".getBytes());
+            System.setIn(in);
+            Game game = new Game();
+
+            assertTrue(true);
+        } catch (Exception e) {
+            ByteArrayInputStream inn = new ByteArrayInputStream("0\n".getBytes());
+
+            try {
+                ByteArrayInputStream innn = new ByteArrayInputStream("нет\n".getBytes());
+                System.setIn(innn);
+                assertTrue(true);
+            } catch (Exception ex) {
+                ByteArrayInputStream innnn = new ByteArrayInputStream("нет\n".getBytes());
+                System.setIn(innnn);
+                assertTrue(true);
+            }
+            System.setIn(inn);
+        } finally {
+            System.setIn(originalIn);
+        }
+        assertTrue(true);
+    }
+}
