53ee354e0337eee4ccd54d795b391d050b58cd55
Kirill <k.shelestov@g.nsu.ru>
2024-09-17 22:12:48 +0700

    Start doing lab

---
+++ Task_1_4_1/src/main/java/ru/nsu/shelestov/Main.java
@@ -0,0 +1,258 @@
+package ru.nsu.shelestov;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Перечисление типо контрольных работ.
+ */
+enum ControlType {
+    ASSIGNMENT,
+    TEST,
+    COLLOQUIUM,
+    EXAM,
+    DIFFERENTIAL_CREDIT,
+    CREDIT,
+    PRACTICE_REPORT_DEFENSE,
+    THESIS_DEFENSE
+}
+
+/**
+ * Класс для хранения информации об оценке за контрольную работу.
+ */
+class Grade {
+    private String subject;
+    private ControlType controlType;
+    private int score;
+
+    /**
+     * Конструктор класса оценки.
+     *
+     * @param subject предмет
+     * @param controlType тип работы
+     * @param score оценка
+     */
+    public Grade(String subject, ControlType controlType, int score) {
+        this.subject = subject;
+        this.controlType = controlType;
+        this.score = score;
+    }
+
+    /**
+     * Геттер оценки.
+     *
+     * @return оценка
+     */
+    public int getScore() {
+        return score;
+    }
+
+    /**
+     * Геттер типа работы.
+     *
+     * @return тип работы
+     */
+    public  ControlType getControlType() {
+        return  controlType;
+    }
+
+    /**
+     * Метод на проверку типа работы - экзамен.
+     *
+     * @return является ли работа экзаменом
+     */
+    public boolean isExam() {
+        return controlType == ControlType.EXAM;
+    }
+
+    /**
+     * Метод на проверку типа работы - зачет/дифзачет.
+     *
+     * @return является ли работа каким-то зачетом
+     */
+    public boolean isCredit() {
+        return controlType == ControlType.CREDIT || controlType == ControlType.DIFFERENTIAL_CREDIT;
+    }
+}
+
+/**
+ * Класс для хранения и обработки успеваемости студента.
+ */
+class StudentRecordBook {
+    List<List<Grade>> semesters; // 8 семестров
+    private boolean isNotPaid; // false если платное, true если бюджет
+
+    /**
+     * Конструктор класса.
+     *
+     * @param isNotPaid платка/бюджет
+     */
+    public StudentRecordBook(boolean isNotPaid) {
+        this.semesters = new ArrayList<>(8);
+        for (int i = 0; i < 8; i++) {
+            semesters.add(new ArrayList<>());
+        }
+        this.isNotPaid = isNotPaid;
+    }
+
+    /**
+     * Метод для добавления оценки.
+     *
+     * @param semester семестр
+     * @param subject предмет
+     * @param controlType тип контрольной работы
+     * @param score оценка
+     */
+    public void addGrade(int semester, String subject, ControlType controlType, Object score) {
+        if (semester < 1 || semester > 8) {
+            throw new IllegalArgumentException("Числа от 1 до 8");
+        }
+        if (controlType == ControlType.CREDIT) {
+            if (!(score instanceof Boolean)) {
+                throw new IllegalArgumentException("Для зачета необходимо передать boolean значение");
+            }
+            boolean scoreBool = (boolean) score;
+            int scoreInt = scoreBool? 5 : 2;
+            semesters.get(semester - 1).add(new Grade(subject, controlType, scoreInt));
+        } else {
+            if (!(score instanceof Integer)) {
+                throw new IllegalArgumentException("Для остальных необходимо передать int значение");
+            }
+            int scoreInt = (int) score;
+            semesters.get(semester - 1).add(new Grade(subject, controlType, scoreInt));
+        }
+    }
+
+    /**
+     * Метод для подсчета среднего балла.
+     *
+     * @return средний балл
+     */
+    public double calculateGPA() {
+        int totalScore = 0;
+        int totalGrades = 0;
+
+        for (List<Grade> semester : semesters) {
+            for (Grade grade : semester) {
+                if (grade.getControlType()!= ControlType.CREDIT) {
+                    totalScore += grade.getScore();
+                    totalGrades++;
+                }
+            }
+        }
+
+        return totalGrades == 0? 0 : (double) totalScore / totalGrades;
+    }
+
+    /**
+     * Метод на проверку перевода на бюджет.
+     *
+     * @param currentSemester текущий семестр
+     * @return возможен ли перевод
+     */
+    public boolean canTransferToBudget(int currentSemester) {
+
+        if (isNotPaid) {
+            return  false;
+        }
+        if (currentSemester < 2) {
+            return  false;
+        }
+        int failedExamsCount = 0;
+        for (int i = (currentSemester - 2); i < currentSemester; i++) {
+            List<Grade> semester = semesters.get(i);
+            for (Grade grade : semester) {
+                if (grade.isExam() && grade.getScore() < 3) {
+                    failedExamsCount++;
+                }
+                if (grade.getControlType() == ControlType.DIFFERENTIAL_CREDIT) {
+                    if (grade.isCredit() && grade.getScore() == 2) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return !isNotPaid && failedExamsCount == 0;
+    }
+
+    /**
+     * Метод для проверки возможности получения красного диплома.
+     *
+     * @return возможно ли получение диплома
+     */
+    public boolean canGetRedDiploma() {
+        int excellentCount = 0;
+        int totalCount = 0;
+        boolean hasUnsatisfactory = false;
+        boolean greatGrade = false;
+        boolean isEightthSemester = false;
+        for (List<Grade> semester : semesters) {
+            for (Grade grade : semester) {
+                if (grade.getScore() < 4) {
+                    hasUnsatisfactory = true;
+                }
+                if (grade.getScore() >= 4) {
+                    excellentCount++;
+                }
+                if (grade.getControlType() == ControlType.THESIS_DEFENSE) {
+                    isEightthSemester = true;
+                    if (grade.getScore() == 5) {
+                        greatGrade = true;
+                    }
+                }
+                totalCount++;
+            }
+        }
+
+        if (isEightthSemester) {
+            if (greatGrade) {
+                return !hasUnsatisfactory && (excellentCount * 100.0 / totalCount >= 75);
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            return !hasUnsatisfactory && (excellentCount * 100.0 / totalCount >= 75);
+        }
+    }
+
+    /**
+     * Метод для проверки возможности получения повышенной стипендии.
+     *
+     * @param currentSemester текущий семестр
+     * @return возможно ли получение стипендии.
+     */
+    public boolean canGetIncreasedScholarship(int currentSemester) {
+
+        if (!isNotPaid) {
+            return false;
+        }
+
+        List<Grade> semesterGrades = semesters.get(currentSemester-1);
+
+        boolean hasExcellent = semesterGrades.stream().anyMatch(grade -> grade.getScore() >= 4);
+
+        return hasExcellent && semesterGrades.stream().noneMatch(grade -> grade.getScore() <= 3);
+    }
+
+
+}
+
+//public class Main {
+//    public static void main(String[] args) {
+//        StudentRecordBook recordBook = new StudentRecordBook(true);
+//
+//        recordBook.addGrade(1, "Math", ControlType.EXAM, 5);
+//        recordBook.addGrade(1, "Physics", ControlType.TEST, 5);
+//        recordBook.addGrade(1, "Chemistry", ControlType.COLLOQUIUM, 5);
+//        recordBook.addGrade(1, "Biology", ControlType.CREDIT, true);
+//        recordBook.addGrade(1, "Biology", ControlType.CREDIT, true);
+//
+//        recordBook.addGrade(8, "VKR", ControlType.THESIS_DEFENSE, 5);
+//        System.out.println("Текущий средний балл: " + recordBook.calculateGPA());
+//        System.out.println("Можно перевести на бюджет: " + recordBook.canTransferToBudget(1));
+//        System.out.println("Можно получить красный диплом: " + recordBook.canGetRedDiploma());
+//        System.out.println("Можно получить повышенную стипендию: " + recordBook.canGetIncreasedScholarship(1));
+//    }
+//}
---
+++ Task_1_4_1/src/test/java/ru/nsu/shelestov/StudentRecordBookTest.java
@@ -0,0 +1,145 @@
+package ru.nsu.shelestov;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Класс для тестирования.
+ */
+public class StudentRecordBookTest {
+
+    /**
+     * Тест на подсчет среднего балла.
+     */
+    @Test
+    public void testCalculateGPA() {
+        StudentRecordBook recordBook = new StudentRecordBook(true);
+        recordBook.addGrade(1, "Math", ControlType.EXAM, 5);
+        recordBook.addGrade(1, "Physics", ControlType.TEST, 4);
+        assertEquals(4.5, recordBook.calculateGPA(), 0.01);
+    }
+
+    /**
+     * Тест на возможность перевода на бюджет.
+     */
+    @Test
+    public void testCanTransferToBudget() {
+        StudentRecordBook recordBook = new StudentRecordBook(false);
+        recordBook.addGrade(1, "Math", ControlType.EXAM, 5);
+        recordBook.addGrade(2, "Physics", ControlType.EXAM, 4);
+        assertTrue(recordBook.canTransferToBudget(3));
+    }
+
+    /**
+     * Тест на возможность перевода на бюджет из-за того, что уже на бюджете.
+     */
+    @Test
+    public void testCannotTransferToBudget() {
+        StudentRecordBook recordBook = new StudentRecordBook(true);
+        recordBook.addGrade(1, "Math", ControlType.EXAM, 5);
+        recordBook.addGrade(2, "Physics", ControlType.EXAM, 4);
+        assertFalse(recordBook.canTransferToBudget(3));
+    }
+
+    /**
+     * Тест на возможность полученмя красного дипдома.
+     */
+    @Test
+    public void testCanGetRedDiploma() {
+        StudentRecordBook recordBook = new StudentRecordBook(false);
+        recordBook.addGrade(1, "Math", ControlType.EXAM, 5);
+        recordBook.addGrade(2, "Physics", ControlType.EXAM, 5);
+        recordBook.addGrade(8, "VKR", ControlType.THESIS_DEFENSE, 5);
+        assertTrue(recordBook.canGetRedDiploma());
+    }
+
+    /**
+     * Тест на невозможность полученмя красного дипдома из-за "3".
+     */
+    @Test
+    public void testCannotGetRedDiploma() {
+        StudentRecordBook recordBook = new StudentRecordBook(true);
+        recordBook.addGrade(1, "Math", ControlType.EXAM, 5);
+        recordBook.addGrade(2, "Physics", ControlType.EXAM, 3);
+        recordBook.addGrade(8, "VKR", ControlType.THESIS_DEFENSE, 5);
+        assertFalse(recordBook.canGetRedDiploma());
+    }
+
+    /**
+     * Тест на невозможность полученмя красного дипдома из-за ВКР.
+     */
+    @Test
+    public void testCannotGetRedDiplomaAgain() {
+        StudentRecordBook recordBook = new StudentRecordBook(true);
+        recordBook.addGrade(1, "Math", ControlType.EXAM, 5);
+        recordBook.addGrade(2, "Physics", ControlType.EXAM, 5);
+        recordBook.addGrade(8, "VKR", ControlType.THESIS_DEFENSE, 4);
+        assertFalse(recordBook.canGetRedDiploma());
+    }
+
+    /**
+     * Тест на возможность получения стипендии.
+     */
+    @Test
+    public void testCanGetIncreasedScholarship() {
+        StudentRecordBook recordBook = new StudentRecordBook(true);
+        recordBook.addGrade(1, "Math", ControlType.EXAM, 5);
+        recordBook.addGrade(1, "Physics", ControlType.TEST, 5);
+        assertTrue(recordBook.canGetIncreasedScholarship(1));
+    }
+
+    /**
+     * Тест на невозможность получения стипендии из-за "3".
+     */
+    @Test
+    public void testCannotGetIncreasedScholarship() {
+        StudentRecordBook recordBook = new StudentRecordBook(true);
+        recordBook.addGrade(1, "Math", ControlType.TEST, 3);
+        recordBook.addGrade(1, "Physics", ControlType.TEST, 4);
+        assertFalse(recordBook.canGetIncreasedScholarship(1));
+    }
+
+    /**
+     * Тест на невозможность получения стипендии из-за того, что на платке.
+     */
+    @Test
+    public void testCannotGetIncreasedScholarshipAgain() {
+        StudentRecordBook recordBook = new StudentRecordBook(false);
+        recordBook.addGrade(1, "Math", ControlType.EXAM, 5);
+        recordBook.addGrade(1, "Physics", ControlType.TEST, 5);
+        assertFalse(recordBook.canGetIncreasedScholarship(1));
+    }
+
+    /**
+     * Тест на добавление оценки.
+     */
+    @Test
+    public void testAddGrade() {
+        StudentRecordBook recordBook = new StudentRecordBook(true);
+        recordBook.addGrade(1, "Math", ControlType.EXAM, 5);
+        assertEquals(1, recordBook.semesters.get(0).size());
+    }
+
+    /**
+     * Тест на добавление оценки в несуществуеющий семестр.
+     */
+    @Test
+    public void testAddGradeInvalidSemester() {
+        StudentRecordBook recordBook = new StudentRecordBook(true);
+        assertThrows(IllegalArgumentException.class, () -> recordBook.addGrade(9, "Math", ControlType.EXAM, 5));
+    }
+
+    /**
+     * Тест на добавление оценки, только с неправильной едом из домаю
+     */
+    @Test
+    public void testAddGradeInvalidScore() {
+        StudentRecordBook recordBook = new StudentRecordBook(true);
+        assertThrows(IllegalArgumentException.class, () -> recordBook.addGrade(1, "Math", ControlType.EXAM, "abc"));
+    }
+}
