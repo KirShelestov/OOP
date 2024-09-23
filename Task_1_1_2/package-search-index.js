39ce4b15369bfbea2fecb438981b19dbaf17f3f5
KirShelestov <KirShelestov@users.noreply.github.com>
2024-09-23 14:15:32 +0000

    Deploying to gh-pages from @ KirShelestov/OOP@492c0e5205ec5838cc8fa88dcabdfa50a9f88197 🚀

--- Task_1_1_2/package-search-index.js
+++ Task_1_1_2/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.shelestov.card"},{"l":"ru.nsu.shelestov.gamelogic"},{"l":"ru.nsu.shelestov.players"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.shelestov.card"},{"l":"ru.nsu.shelestov.gamelogic"},{"l":"ru.nsu.shelestov.players"}];updateSearchResults();
