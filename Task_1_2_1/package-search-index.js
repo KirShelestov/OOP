88f86867904000c968f02189d84f6916e81d9878
KirShelestov <KirShelestov@users.noreply.github.com>
2024-10-15 04:18:48 +0000

    Deploying to gh-pages from @ KirShelestov/OOP@00cea4f6751aa4be67a816f7c0f7dd1e2f22e82d 🚀

--- Task_1_2_1/package-search-index.js
+++ Task_1_2_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.shelestov"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.shelestov.graph"}];updateSearchResults();
