12859a63709c5fb1fa4e16d34affe4f26dbbb67b
Kirill <k.shelestov@g.nsu.ru>
2024-11-05 15:33:25 +0700

    Fix checkstyle

--- Task_1_2_2/src/test/java/ru/nsu/shelestov/hashtable/HashTableTest.java
+++ Task_1_2_2/src/test/java/ru/nsu/shelestov/hashtable/HashTableTest.java
@@ -4,7 +4,11 @@ import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Класс для тестирования хеш-таблицы.
