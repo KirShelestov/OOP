77447e828562d3125b92a402a0964cf1bf79004c
KirShelestov <KirShelestov@users.noreply.github.com>
2024-11-26 16:34:11 +0000

    Deploying to gh-pages from @ KirShelestov/OOP@83459400a1d47d61e01d19ed3d7f813d286d7e63 🚀

--- Task_1_4_1/type-search-index.js
+++ Task_1_4_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.shelestov","l":"ControlType"},{"p":"ru.nsu.shelestov","l":"Grade"},{"p":"ru.nsu.shelestov","l":"StudentRecordBook"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.shelestov.recordbook","l":"ControlType"},{"p":"ru.nsu.shelestov.recordbook","l":"Grade"},{"p":"ru.nsu.shelestov.recordbook","l":"StudentRecordBook"}]
