5a50f5a4fb32c64813403cb6577eb68dbb990885
Kirill <k.shelestov@g.nsu.ru>
2024-11-26 23:47:54 +0700

    Fix checkstyle

--- Task_1_4_1/src/main/java/ru/nsu/shelestov/recordbook/StudentRecordBook.java
+++ Task_1_4_1/src/main/java/ru/nsu/shelestov/recordbook/StudentRecordBook.java
@@ -1,188 +1,181 @@
-    package ru.nsu.shelestov.recordbook;
+package ru.nsu.shelestov.recordbook;
 
-    import java.util.ArrayList;
-    import java.util.HashMap;
-    import java.util.List;
-    import java.util.Map;
-    import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+/**
+ * Класс для хранения и обработки успеваемости студента.
+ */
+public class StudentRecordBook {
+    private List<Map<ControlType, Integer>> maxGradesPerSemester;
+    List<Map<ControlType, List<Grade>>> semesters; // 8 семестров
+    private boolean isNotPaid; // false если платное, true если бюджет
 
     /**
-     * Класс для хранения и обработки успеваемости студента.
+     * Конструктор класса.
+     *
+     * @param isNotPaid платка/бюджет
+     * @param semesterConfigs список конфигураций семестров, где каждая конфигурация - это карта типа контроля и количества оценок
      */
-    public class StudentRecordBook {
-        private List<Map<ControlType, Integer>> maxGradesPerSemester;
-        List<Map<ControlType, List<Grade>>> semesters; // 8 семестров
-        private boolean isNotPaid; // false если платное, true если бюджет
+    public StudentRecordBook(boolean isNotPaid, List<Map<ControlType, Integer>> semesterConfigs) {
+        this.semesters = new ArrayList<>();
+        this.maxGradesPerSemester = new ArrayList<>();
 
-        /**
-         * Конструктор класса.
-         *
-         * @param isNotPaid платка/бюджет
-         * @param semesterConfigs список конфигураций семестров, где каждая конфигурация - это карта типа контроля и количества оценок
-         */
-        public StudentRecordBook(boolean isNotPaid, List<Map<ControlType, Integer>> semesterConfigs) {
-            this.semesters = new ArrayList<>();
-            this.maxGradesPerSemester = new ArrayList<>();
-
-            for (Map<ControlType, Integer> controlTypeMap : semesterConfigs) {
-                Map<ControlType, List<Grade>> semester = new HashMap<>();
-                for (Map.Entry<ControlType, Integer> entry : controlTypeMap.entrySet()) {
-                    semester.put(entry.getKey(), new ArrayList<>());
-                }
-                this.semesters.add(semester);
-                this.maxGradesPerSemester.add(controlTypeMap);
+        for (Map<ControlType, Integer> controlTypeMap : semesterConfigs) {
+            Map<ControlType, List<Grade>> semester = new HashMap<>();
+            for (Map.Entry<ControlType, Integer> entry : controlTypeMap.entrySet()) {
+                semester.put(entry.getKey(), new ArrayList<>());
             }
+            this.semesters.add(semester);
+            this.maxGradesPerSemester.add(controlTypeMap);
         }
-
-        /**
-         * Метод для добавления оценки.
-         *
-         * @param semester семестр
-         * @param subject предмет
-         * @param controlType тип контрольной работы
-         * @param score оценка
-         */
-        public void addGrade(int semester, String subject, ControlType controlType, Object score) {
-            if (semester < 1 || semester > semesters.size()) {
-                throw new IllegalArgumentException("Числа от 1 до " + semesters.size());
-            }
-            Map<ControlType, List<Grade>> controlTypeMap = semesters.get(semester - 1);
-            List<Grade> grades = controlTypeMap.get(controlType);
-
-            boolean subjectExists = controlTypeMap.values().stream()
-                    .flatMap(List::stream)
-                    .anyMatch(grade -> grade.getSubject().equals(subject));
-
-            if (subjectExists) {
-                throw new IllegalArgumentException("Предмет " + subject + " уже добавлен в текущем семестре с другим видом контроля.");
-            }
-
-            int maxGrades = maxGradesPerSemester.get(semester - 1).get(controlType); // Get the max from the config
-
-            if (grades.size() >= maxGrades) {
-                throw new IllegalArgumentException("Достигнуто максимальное количество оценок для данного типа контроля в семестре");
-            }
-
-            if (controlType == ControlType.CREDIT) {
-                if (!(score instanceof Boolean)) {
-                    throw new IllegalArgumentException("Для зачета необходимо передать boolean значение");
-                }
-                boolean scoreBool = (boolean) score;
-                int scoreInt = scoreBool ? 5 : 2;
-                grades.add(new Grade(subject, controlType, scoreInt));
-            } else {
-                if (!(score instanceof Integer)) {
-                    throw new IllegalArgumentException("Для остальных необходимо передать int значение");
-                }
-                int scoreInt = (int) score;
-                grades.add(new Grade(subject, controlType, scoreInt));
-            }
-        }
-
-
-
-        /**
-         * Метод для подсчета среднего балла.
-         *
-         * @return средний балл
-         */
-        public double calculateGpa() {
-            return semesters.stream()
-                    .flatMap(controlTypeMap -> controlTypeMap.values().stream())
-                    .flatMap(List::stream)
-                    .filter(grade -> grade.getControlType() != ControlType.CREDIT)
-                    .mapToInt(Grade::getScore)
-                    .average()
-                    .orElse(0);
-        }
-
-
-        /**
-         * Метод на проверку перевода на бюджет.
-         *
-         * @param currentSemester текущий семестр
-         * @return возможен ли перевод
-         */
-        public boolean canTransferToBudget(int currentSemester) {
-            if (isNotPaid || currentSemester < 2) {
-                return false;
-            }
-
-            long failedExamsCount = semesters.subList(currentSemester - 2, currentSemester).stream()
-                    .flatMap(controlTypeMap -> controlTypeMap.values().stream())
-                    .flatMap(List::stream)
-                    .filter(grade -> grade.isExam() && grade.getScore() < 3)
-                    .count();
-
-            boolean hasUnsatisfactoryCredits = semesters.subList(currentSemester - 2, currentSemester).stream()
-                    .flatMap(controlTypeMap -> controlTypeMap.values().stream())
-                    .flatMap(List::stream)
-                    .anyMatch(grade -> grade.getControlType() == ControlType.DIFFERENTIAL_CREDIT && grade.isCredit() && grade.getScore() == 2);
-
-            return failedExamsCount == 0 && !hasUnsatisfactoryCredits;
-        }
-
-
-        /**
-         * Метод для проверки возможности получения красного диплома.
-         *
-         * @return возможно ли получение диплома
-         */
-        public boolean canGetRedDiploma() {
-            long totalCount = semesters.stream()
-                    .flatMap(controlTypeMap -> controlTypeMap.values().stream())
-                    .flatMap(List::stream)
-                    .count();
-
-            long excellentCount = semesters.stream()
-                    .flatMap(controlTypeMap -> controlTypeMap.values().stream())
-                    .flatMap(List::stream)
-                    .filter(grade -> grade.getScore() >= 4)
-                    .count();
-
-            boolean hasUnsatisfactory = semesters.stream()
-                    .flatMap(controlTypeMap -> controlTypeMap.values().stream())
-                    .flatMap(List::stream)
-                    .anyMatch(grade -> grade.getScore() < 4);
-
-            boolean greatGrade = semesters.stream()
-                    .flatMap(controlTypeMap -> controlTypeMap.values().stream())
-                    .flatMap(List::stream)
-                    .filter(grade -> grade.getControlType() == ControlType.THESIS_DEFENSE)
-                    .anyMatch(grade -> grade.getScore() == 5);
-
-            boolean isEighthSemester = semesters.stream()
-                    .flatMap(controlTypeMap -> controlTypeMap.values().stream())
-                    .flatMap(List::stream)
-                    .anyMatch(grade -> grade.getControlType() == ControlType.THESIS_DEFENSE);
-
-            return (isEighthSemester && greatGrade && !hasUnsatisfactory && (excellentCount * 100.0 / totalCount >= 75)) ||
-                    (!isEighthSemester && !hasUnsatisfactory && (excellentCount * 100.0 / totalCount >= 75));
-        }
-
-
-        /**
-         * Метод для проверки возможности получения повышенной стипендии.
-         *
-         * @param currentSemester текущий семестр
-         * @return возможно ли получение стипендии.
-         */
-        public boolean canGetIncreasedScholarship(int currentSemester) {
-            if (!isNotPaid) {
-                return false;
-            }
-
-            Map<ControlType, List<Grade>> controlTypeMap = semesters.get(currentSemester);
-
-            List<Grade> semesterGrades = controlTypeMap.values().stream()
-                    .flatMap(List::stream)
-                    .collect(Collectors.toList());
-
-            boolean hasExcellent = semesterGrades.stream().anyMatch(grade -> grade.getScore() >= 4);
-            boolean hasUnsatisfactory = semesterGrades.stream().anyMatch(grade -> grade.getScore() <= 3);
-
-            return hasExcellent && (!hasUnsatisfactory);
-        }
-
     }
+
+    /**
+     * Метод для добавления оценки.
+     *
+     * @param semester семестр
+     * @param subject предмет
+     * @param controlType тип контрольной работы
+     * @param score оценка
+     */
+    public void addGrade(int semester, String subject, ControlType controlType, Object score) {
+        if (semester < 1 || semester > semesters.size()) {
+            throw new IllegalArgumentException("Числа от 1 до " + semesters.size());
+        }
+        Map<ControlType, List<Grade>> controlTypeMap = semesters.get(semester - 1);
+        List<Grade> grades = controlTypeMap.get(controlType);
+
+        boolean subjectExists = controlTypeMap.values().stream()
+                .flatMap(List::stream)
+                .anyMatch(grade -> grade.getSubject().equals(subject));
+
+        if (subjectExists) {
+            throw new IllegalArgumentException("Предмет " + subject + " уже добавлен в текущем семестре с другим видом контроля.");
+        }
+
+        int maxGrades = maxGradesPerSemester.get(semester - 1).get(controlType); // Get the max from the config
+
+        if (grades.size() >= maxGrades) {
+            throw new IllegalArgumentException("Достигнуто максимальное количество оценок для данного типа контроля в семестре");
+        }
+
+        if (controlType == ControlType.CREDIT) {
+            if (!(score instanceof Boolean)) {
+                throw new IllegalArgumentException("Для зачета необходимо передать boolean значение");
+            }
+            boolean scoreBool = (boolean) score;
+            int scoreInt = scoreBool ? 5 : 2;
+            grades.add(new Grade(subject, controlType, scoreInt));
+        } else {
+            if (!(score instanceof Integer)) {
+                throw new IllegalArgumentException("Для остальных необходимо передать int значение");
+            }
+            int scoreInt = (int) score;
+            grades.add(new Grade(subject, controlType, scoreInt));
+        }
+    }
+
+    /**
+     * Метод для подсчета среднего балла.
+     *
+     * @return средний балл
+     */
+    public double calculateGpa() {
+        return semesters.stream()
+                .flatMap(controlTypeMap -> controlTypeMap.values().stream())
+                .flatMap(List::stream)
+                .filter(grade -> grade.getControlType() != ControlType.CREDIT)
+                .mapToInt(Grade::getScore)
+                .average()
+                .orElse(0);
+    }
+
+    /**
+     * Метод на проверку перевода на бюджет.
+     *
+     * @param currentSemester текущий семестр
+     * @return возможен ли перевод
+     */
+    public boolean canTransferToBudget(int currentSemester) {
+        if (isNotPaid || currentSemester < 2) {
+            return false;
+        }
+
+        long failedExamsCount = semesters.subList(currentSemester - 2, currentSemester).stream()
+                .flatMap(controlTypeMap -> controlTypeMap.values().stream())
+                .flatMap(List::stream)
+                .filter(grade -> grade.isExam() && grade.getScore() < 3)
+                .count();
+
+        boolean hasUnsatisfactoryCredits = semesters.subList(currentSemester - 2, currentSemester).stream()
+                .flatMap(controlTypeMap -> controlTypeMap.values().stream())
+                .flatMap(List::stream)
+                .anyMatch(grade -> grade.getControlType() == ControlType.DIFFERENTIAL_CREDIT && grade.isCredit() && grade.getScore() == 2);
+
+        return failedExamsCount == 0 && !hasUnsatisfactoryCredits;
+    }
+
+    /**
+     * Метод для проверки возможности получения красного диплома.
+     *
+     * @return возможно ли получение диплома
+     */
+    public boolean canGetRedDiploma() {
+        long totalCount = semesters.stream()
+                .flatMap(controlTypeMap -> controlTypeMap.values().stream())
+                .flatMap(List::stream)
+                .count();
+
+        long excellentCount = semesters.stream()
+                .flatMap(controlTypeMap -> controlTypeMap.values().stream())
+                .flatMap(List::stream)
+                .filter(grade -> grade.getScore() >= 4)
+                .count();
+
+        boolean hasUnsatisfactory = semesters.stream()
+                .flatMap(controlTypeMap -> controlTypeMap.values().stream())
+                .flatMap(List::stream)
+                .anyMatch(grade -> grade.getScore() < 4);
+
+        boolean greatGrade = semesters.stream()
+                .flatMap(controlTypeMap -> controlTypeMap.values().stream())
+                .flatMap(List::stream)
+                .filter(grade -> grade.getControlType() == ControlType.THESIS_DEFENSE)
+                .anyMatch(grade -> grade.getScore() == 5);
+
+        boolean isEighthSemester = semesters.stream()
+                .flatMap(controlTypeMap -> controlTypeMap.values().stream())
+                .flatMap(List::stream)
+                .anyMatch(grade -> grade.getControlType() == ControlType.THESIS_DEFENSE);
+
+        return (isEighthSemester && greatGrade && !hasUnsatisfactory && (excellentCount * 100.0 / totalCount >= 75)) ||
+                (!isEighthSemester && !hasUnsatisfactory && (excellentCount * 100.0 / totalCount >= 75));
+    }
+
+    /**
+     * Метод для проверки возможности получения повышенной стипендии.
+     *
+     * @param currentSemester текущий семестр
+     * @return возможно ли получение стипендии.
+     */
+    public boolean canGetIncreasedScholarship(int currentSemester) {
+        if (!isNotPaid) {
+            return false;
+        }
+
+        Map<ControlType, List<Grade>> controlTypeMap = semesters.get(currentSemester);
+
+        List<Grade> semesterGrades = controlTypeMap.values().stream()
+                .flatMap(List::stream)
+                .collect(Collectors.toList());
+
+        boolean hasExcellent = semesterGrades.stream().anyMatch(grade -> grade.getScore() >= 4);
+        boolean hasUnsatisfactory = semesterGrades.stream().anyMatch(grade -> grade.getScore() <= 3);
+
+        return hasExcellent && (!hasUnsatisfactory);
+    }
+}
